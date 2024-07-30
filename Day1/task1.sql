c0d82c067ab52e0675dc5ab994782c28799b5127
PRATIKBAGADE1 <pratikbagade13@gmail.com>
2024-07-30 11:04:51 +0530

    commit changes

---
+++ Day1/task1.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE students;
+use students;
+create table emp(s_id INT PRIMARY KEY,s_name varchar(50),s_age TINYINT);
+ insert into emp values (1,"Ram",21);
+  insert into emp values (2,"shamam",21);
+   select * from emp;
